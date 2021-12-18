fe83c7c92ab8db9857a9483f1dbaed1e9c9b3448
Tomas Edson <t.edson.almeida@hotmail.com>
2021-12-18 13:41:03 -0300

    feat: num float with . and operator

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const frutas = [];
 const numeral_um = [];
+let numero1 = "";
+let operador = ""
 
 frutas.push("pera");
 frutas.push("maçã");
@@ -17,12 +19,32 @@ function digito(numero){
 
     let tam =  numeral_um.length;
     let num1 = "";
+    //ARMAZENA QUANTAS VEZES O PONTO "." APARECE
+    let ponto = 0;
+    //PERCORRE O ARRAY numeral_um E CADA ITEM ENCONTRADO É CONCATENADO NA VARIÁVEL num1
+    //PARA EM SEGUIDA EXIBIR NA TELA
+    for(const element of numeral_um){
+        
+        
+        //ARMAZENA O elemento atual do array
+        item = element;
 
-    for(var i = 0; i < tam; i++){
+        if (item == "."){
+            ponto = ponto + 1;
 
-        num1 = num1 + numeral_um[i];
+            if (ponto == 1){
+                num1 = num1 + ".";
+            }
 
-        document.getElementById("demo").innerHTML = num1;
+        }else{
+            num1 = num1 + item;
+
+        }
+
+      numero1 = parseFloat(num1);
+        
+
+        document.getElementById("demo").innerHTML = numero1;
         document.getElementById("tela").value = num1;
 
 
@@ -31,6 +53,32 @@ function digito(numero){
     //document.write(num1);
     //document.getElementById("demo").innerHTML = numeral_um;
     // document.write(numeral_um[0]);
+}
+
+//FUNÇÃO ATRIBUI A VARIÁVEL O TIPO DE OPERACÃO
+function operacao(tipo){
+
+    if (numero1 != ""){
+        if (tipo == "soma"){
+            operador = "+";
+        }
+        if (tipo == "menos"){
+            operador = "-";
+        }
+        if (tipo == "multi"){
+            operador = "X";
+        }
+        if (tipo == "divisao"){
+            operador = "/";
+        }
+    
+        let valor = numero1 + operador;
+        document.getElementById("tela").value = valor;
+
+    }
+
+    
+    
 }
 
 

89053c99fa03a629a7fb7c880d8788319d6e94d3
Tomas Edson <t.edson.almeida@hotmail.com>
2021-12-19 18:44:28 -0300

    feat: numeral_dois

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 const frutas = [];
 const numeral_um = [];
 let numero1 = "";
-let operador = ""
+let operador = ""//ARMAZENA TODOS OS ITENS DIGITADOS PARA EXIBIR EM TELA
+let valor = ""
+//VARIÁVELARMAZENA O EVENTO ATUAL, OU SEJA, SE 1, ENTÃO ESTAMOS GRAVANDO NO "NUMERAL_UM"
+//APÓS PRESSIONAR O OPERADOR A VARIÁVEL RECEBERÁ O 2 E PASSAREMOS A GRAVAR NO "NUMERAL_DOIS"
+let evento = 1;
+
+const numeral_dois = [];
+let numero2 = "";
 
 frutas.push("pera");
 frutas.push("maçã");
@@ -15,44 +22,85 @@ frutas.push("goiaba");
 
 function digito(numero){
 
-    numeral_um.push(numero)
+    //ADICIONA ITENS AO NÚMERO 1
+    if (evento == 1){
+        numeral_um.push(numero)
 
-    let tam =  numeral_um.length;
-    let num1 = "";
-    //ARMAZENA QUANTAS VEZES O PONTO "." APARECE
-    let ponto = 0;
-    //PERCORRE O ARRAY numeral_um E CADA ITEM ENCONTRADO É CONCATENADO NA VARIÁVEL num1
-    //PARA EM SEGUIDA EXIBIR NA TELA
-    for(const element of numeral_um){
-        
-        
-        //ARMAZENA O elemento atual do array
-        item = element;
+        let tam =  numeral_um.length;
+        let num1 = "";
+        //ARMAZENA QUANTAS VEZES O PONTO "." APARECE
+        let ponto = 0;
+        //PERCORRE O ARRAY numeral_um E CADA ITEM ENCONTRADO É CONCATENADO NA VARIÁVEL num1
+        //PARA EM SEGUIDA EXIBIR NA TELA
+        for(const element of numeral_um){
+            
+            
+            //ARMAZENA O elemento atual do array
+            item = element;
 
-        if (item == "."){
-            ponto = ponto + 1;
+            if (item == "."){
+                ponto = ponto + 1;
 
-            if (ponto == 1){
-                num1 = num1 + ".";
+                if (ponto == 1){
+                    num1 = num1 + ".";
+                }
+
+            }else{
+                num1 = num1 + item;
+
             }
 
-        }else{
-            num1 = num1 + item;
+        numero1 = parseFloat(num1);
+            
+
+            document.getElementById("demo").innerHTML = numero1;
+            document.getElementById("tela").value = num1;
+
 
         }
 
-      numero1 = parseFloat(num1);
+    }else{
+        //ADICIONA ITENS AO NÚMERO 1
+        numeral_dois.push(numero)
+
+        let tam =  numeral_dois.length;
+        let num2 = "";
+        //ARMAZENA QUANTAS VEZES O PONTO "." APARECE
+        let ponto = 0;
+        //PERCORRE O ARRAY numeral_um E CADA ITEM ENCONTRADO É CONCATENADO NA VARIÁVEL num1
+        //PARA EM SEGUIDA EXIBIR NA TELA
+        for(const element of numeral_dois){
+            
+            
+            //ARMAZENA O elemento atual do array
+            item = element;
+
+            if (item == "."){
+                ponto = ponto + 1;
+
+                if (ponto == 1){
+                    num2 = num2 + ".";
+                }
+
+            }else{
+                num2 = num2 + item;
+
+            }
+
+        numero2 = parseFloat(num2);
+            
+
+            document.getElementById("demo").innerHTML = numero2;
+            document.getElementById("tela").value = valor + " " + numero2;
+
+
+        }
         
 
-        document.getElementById("demo").innerHTML = numero1;
-        document.getElementById("tela").value = num1;
-
-
     }
-    //document.write("<br>");
-    //document.write(num1);
-    //document.getElementById("demo").innerHTML = numeral_um;
-    // document.write(numeral_um[0]);
+    
+    
+    
 }
 
 //FUNÇÃO ATRIBUI A VARIÁVEL O TIPO DE OPERACÃO
@@ -71,8 +119,9 @@ function operacao(tipo){
         if (tipo == "divisao"){
             operador = "/";
         }
-    
-        let valor = numero1 + operador;
+        
+        evento = 2;//A PARITR DE AGORA SERÁ ARMAZENADO OS ITENS NO "NUMERAL_DOIS"
+        valor = numero1 + " " + operador;
         document.getElementById("tela").value = valor;
 
     }
@@ -82,5 +131,16 @@ function operacao(tipo){
 }
 
 
+function limpar(){
 
+    document.getElementById("tela").value = "";
+
+    numero1 = "";
+    numero2 = "";
+    operador = "";
+    evento = 1;
+    //LIMPAR O ARRAY, A PARTIR DA POSIÇÃO "0"
+    numeral_um.splice(0,numeral_um.length);
+    numeral_dois.splice(0,numeral_dois.length);
+}
 
